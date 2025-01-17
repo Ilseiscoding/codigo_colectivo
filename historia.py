9d36d08f7659010cf9c7f1546f9553db0dd2574b
Ilse Aguilar Segoviano <142247901+Ilseiscoding@users.noreply.github.com>
2025-01-17 17:14:05 -0600

    Create historia.py

---
+++ historia.py
@@ -0,0 +1,6 @@
+def historia_colectiva():
+    print("Bienvenidos a la gran historia de nuestros alumnos y Python.")
+    nombre = input("Dime tu nombre (personaje principal): ")
+
+    print(f"{nombre} llegó a la primera clase con mucha curiosidad.")
+    print(f"Empezó a experimentar
